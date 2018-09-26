d738b04c8a89053b59074d675a93bb6dade50c74
Antonia Calia-Bogan <alfriadox@gmail.com>
2018-09-26 11:52:41 -0400

    Hello, World

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
 fn main() {
-    println!("Hello, world!");
+    Iron::new(handler).http("localhost:3000").unwrap();
+}
+
+fn handler(_req: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Hello World!")))
 }
